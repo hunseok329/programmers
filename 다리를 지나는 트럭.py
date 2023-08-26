735b33861c64717fdf581e64700e64c31619ef98
hunseok329 <kha5064@gmail.com>
2023-08-26 23:22:36 +0900

    #solve

--- 다리를 지나는 트럭.py
+++ 다리를 지나는 트럭.py
@@ -13,3 +13,45 @@ def solution(bridge_length, weight, truck_weights):
             bridge_weight -= bridge[0]
             del bridge[0]
     return count + bridge_length
+
+
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    bridge = deque()
+    cnt = 0
+    bridge_weigths = 0
+    truck_weights.reverse()
+
+    while truck_weights:
+        cnt += 1
+        if bridge_weigths + truck_weights[-1] <= weight:
+            bridge_weigths += truck_weights[-1]
+            bridge.append(truck_weights.pop())
+        else:
+            bridge.append(0)
+        if len(bridge) == bridge_length:
+            bridge_weigths -= bridge.popleft()
+    return cnt + bridge_length
+
+
+
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    bridge = deque([0] * bridge_length)
+    cnt = 0
+    bridge_weigths = 0
+    truck_weights.reverse()
+
+    while truck_weights:
+        bridge_weigths -= bridge.popleft()
+        cnt += 1
+        
+        if bridge_weigths + truck_weights[-1] <= weight:
+            bridge_weigths += truck_weights[-1]
+            bridge.append(truck_weights.pop())
+        else:
+            bridge.append(0)
+            
+    return cnt + bridge_length
