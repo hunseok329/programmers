1788d27af6da62e2148f230998e0f5c58b5d1037
hunseok329 <kha5064@gmail.com>
2023-08-31 22:41:22 +0900

    #solve

--- 정수 삼각형.py
+++ 정수 삼각형.py
@@ -6,3 +6,13 @@ def solution(triangle):
             else:
                 triangle[i-1][j] += triangle[i][j+1]
     return triangle[0][0]
+
+
+def solution(triangle):
+    for high in range(len(triangle)-1, 0, -1):
+        for row in range(len(triangle[high])-1):
+            if triangle[high][row] < triangle[high][row+1]:
+                triangle[high-1][row] += triangle[high][row+1]
+            else:
+                triangle[high-1][row] += triangle[high][row]
+    return triangle[0][0]
